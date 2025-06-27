8567d45b3a663e15f425918cbb71153b5cb45ca5
Gregus31 <gregoryhuet02@hotmail.fr>
2025-06-27 10:25:43 +0200

    Update Salle.java

--- Salle.java
+++ Salle.java
@@ -1,1 +1,45 @@
+import java.util.Random;
+
+public class Salle {
+    private boolean estSortie;
+    private String description;
+    private Arme arme;
+    private Ennemi ennemi;
+
+    public Salle(boolean estSortie, String description) {
+        this.estSortie = estSortie;
+        this.description = description;
+        this.arme = null;
+        this.ennemi = null;
+    }
+
+    public boolean estSortie() {
+        return estSortie;
+    }
+
+    public void afficherDescription() {
+        System.out.println(description);
+    }
+
+    public Arme getArme() {
+        return arme;
+    }
+
+    public void setArme(Arme arme) {
+        this.arme = arme;
+    }
+    public void setEnnemi(int x){
+        if (x==0){
+            this.ennemi=null;
+        }else{
+        Random rn = new Random();
+        if (rn.nextBoolean()){
+            this.ennemi = new Ennemi();
+        }
+    }
+    }
+    public Ennemi getEnnemi(){
+        return this.ennemi;
+    }
+}
 
